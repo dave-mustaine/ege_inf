fca12aaee201c5c441ba69b11a84126ea8130753
sveta <s.petruhina@student.univers.su>
2024-02-15 12:45:19 +0700

    :)

--- 23_task_ege.py
+++ 23_task_ege.py
@@ -1,4 +1,34 @@
+# def f(a, b):
+#     if a == 10:
+#         return 0  # если не заходить в точку
+#     if a == b:
+#         return 1
+#     if a > b:
+#         return 0
+#     if a < b:
+#         return f(a + 1, b) + f(a * 2, b)
+#
+#
+# print(f(1, 28))
+
+
+# def f(a, b):
+#     # if a == 10:
+#     #     return 0  # если не заходить в точку
+#     if a == b:
+#         return 1
+#     if a > b:
+#         return 0
+#     if a < b:
+#         return f(a + 1, b) + f(a * 2, b)
+#
+#
+# print(f(1, 10) * f(10, 20))
+
+
 def f(a, b):
+    if a == 11:
+        return 0  # если не заходить в точку
     if a == b:
         return 1
     if a > b:
@@ -7,4 +37,4 @@ def f(a, b):
         return f(a + 1, b) + f(a * 2, b)
 
 
-print(f(1, 28))
+print(f(1, 10) * f(10, 20))
