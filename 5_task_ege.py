a1795ee7fbea736003e7dc6652ff595951a3b3f8
Svetlana <70884323+dave-mustaine@users.noreply.github.com>
2024-02-15 12:29:01 +0700

    Add files via upload

---
+++ 5_task_ege.py
@@ -0,0 +1,18 @@
+R = 0
+numberr = 0
+
+while R < 76:
+    numberr += 1
+
+    n_bits = str(bin(numberr))[2::]
+
+    if not (numberr % 3):
+        n_bits = n_bits + n_bits[-3:]
+    else:
+        a = (numberr % 3) * 3
+        n_bits = n_bits + str(bin(a))[2::]
+
+    R = int(n_bits, 2)
+
+print(numberr - 1)
+#  dlijeld;w
