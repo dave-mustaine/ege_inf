f621a55fa3d000f681367354dfa43fa78193319c
sveta <s.petruhina@student.univers.su>
2024-02-19 17:09:11 +0700

    :)

--- 14_task_ege.py
+++ 14_task_ege.py
@@ -83,25 +83,116 @@
 #
 # print(sum(r))
 
-# длинная арифметика
-# https://education.yandex.ru/ege/task/39991489-2021-4ee7-96a1-f45152dbfcd2
-def counterr():
-    x = 0
-    raznost = 0
+# # длинная арифметика РАЗОБРАТЬ
+# # https://education.yandex.ru/ege/task/39991489-2021-4ee7-96a1-f45152dbfcd2
+# def counterr():
+#     x = 0
+#     raznost = 0
+#
+#     while raznost != 471:
+#         s = 9 ** 1_942 + 9 * 6 ** 971 + 214 - x
+#         s = abs(s)
+#
+#         r = []
+#
+#         while s > 0:
+#             r.append(s % 9)
+#             s //= 9
+#
+#         raznost = abs(r.count(2) - r.count(8))
+#
+#     return x
+#
+#
+# print(counterr())
 
-    while raznost != 471:
-        s = 9 ** 1_942 + 9 * 6 ** 971 + 214 - x
-        s = abs(s)
+# # выражение с неизвестными цифрами
+# # https://education.yandex.ru/ege/task/e07ffce3-c3fb-4aad-933f-4291ee998760
+# for x in '0123456789abcd':
+#     for y in '0123456789abcd':
+#         s = int('14' + y + '5' + x + '2', 14) + int('31' + x + '2' + y + '3', 14)
+#
+#         if s % 9 == 0:
+#             print(x, y, s / 9)
 
-        r = []
+# # выражение с неизвестными цифрами
+# # https://education.yandex.ru/ege/task/749a92f0-0083-4931-90cf-8c987a48ed9c
+# x_y = []
+#
+# for x in '0123456789abcdef':
+#     for y in '0123456789abcdef':
+#         s = int('27a' + x + '23', 16) + int('8' + y + 'e5d2', 16)
+#
+#         if s % 5 == 0:
+#             x_y.append(int(x, 16) + int(y, 16))
+#
+# print(max(x_y))
 
-        while s > 0:
-            r.append(s % 9)
-            s //= 9
+# # выражение с неизвестными цифрами
+# # https://education.yandex.ru/ege/task/376fdf5f-fe0f-49fe-873c-698126bf1ccd
+#
+# for x in '0123456789abcdefghijklmn':
+#     for y in '0123456789abcdefghijklmn':
+#         s = (int('4m' + x + 'f', 24) + int('265afdn' + x, 24) + int('n4' + x + '931b3l', 24)
+#              + int('ng' + x + '4f', 24) + int('fkjb5' + x + 'ik', 24))
+#
+#         if s % 23 == 0:
+#             print(s / 23)
+#             break
 
-        raznost = abs(r.count(2) - r.count(8))
+# # выражение с неизвестными цифрами РАЗОБРАТЬ
+# # https://education.yandex.ru/ege/task/376fdf5f-fe0f-49fe-873c-698126bf1ccd
+# ss = []
+# # int('4' + y, int(x))
+# for x in '01234567':
+#     for y in '01234567':
+#         s = int('36' + x + '53', 8) - (4 * int(x) + int(y))
+#         ss.append(s)
+#
+# print(min(ss))
 
-    return x
+# # выражение с неизвестными цифрами
+# # https://education.yandex.ru/ege/task/efccf0c5-008f-4ac1-a586-9973538f9523
+# ss = []
+# for x in '0123456789abcdefghijklmno':
+#     for y in '0123456789abcdefghijklmno':
+#         s = int('b7' + x + '35f1', 25) + int('16' + x + 'k1', 25) + int('1' + x + 'g', 25)
+#         if s % 24 == 0:
+#             ss.append(s)
+#
+# print(max(ss) // 24)
 
+# # выражение с неизвестными цифрами
+# # https://education.yandex.ru/ege/task/258db83f-773f-43a0-88da-89f8e3c2ab70
+# ss = []
+# for x in '0123456789abcdefgh':
+#     s = int('77968' + x + '11', 18) + int('4' + x + '213', 18)
+#     if s % 7 == 0:
+#         ss.append(x)
+#
+# print((int('77968' + max(ss) + '11', 18) + int('4' + max(ss) + '213', 18)) / 7)
 
-print(counterr())
+# # длинная арифметика
+# # https://education.yandex.ru/ege/task/1273d7d9-0226-4742-99da-70fc4876a4eb
+# s = 7 * 5 ** 123 + 6 * 5 ** 111 - 5 * 25 ** 50 + 4 * 125 ** 30 - 3 * 5 ** 10
+# s = abs(s)
+#
+# r = []
+#
+# while s > 0:
+#     r.append(s % 5)
+#     s //= 5
+#
+# print(r.count(4))
+
+# # длинная арифметика
+# # https://education.yandex.ru/ege/task/6e6c2557-26e8-4b87-a6ff-96defe05f178
+# s = 5 ** 20 + 5 ** 10 - 5 ** 13 - 5 ** 3
+#
+# r = []
+#
+# while s > 0:
+#     r.append(s % 5)
+#     s //= 5
+#
+# print(sum(r))
